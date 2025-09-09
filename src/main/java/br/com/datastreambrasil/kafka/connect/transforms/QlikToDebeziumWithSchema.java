dd25bc76da5ac74a0f315e8584439de5cb3667b4
Afonso Lima <afonso.lima@datastreambrasil.com.br>
2025-09-09 11:27:34 -0300

    Atualiza classe QlikToDebeziumWithSchema

--- src/main/java/br/com/datastreambrasil/kafka/connect/transforms/QlikToDebeziumWithSchema.java
+++ src/main/java/br/com/datastreambrasil/kafka/connect/transforms/QlikToDebeziumWithSchema.java
@@ -4,6 +4,7 @@ import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.transforms.Transformation;
 
 import java.util.*;
@@ -35,46 +36,58 @@ public class QlikToDebeziumWithSchema<R extends ConnectRecord<R>> implements Tra
         Map<String, Object> beforeData = (Map<String, Object>) qlikMessage.get("beforeData");
         Map<String, Object> headers = (Map<String, Object>) qlikMessage.get("headers");
 
-        // --- 1. Construir schema dinâmico ---
-        SchemaBuilder schemaBuilder = SchemaBuilder.struct().name("QlikRecord");
-        for (Map.Entry<String, Object> entry : data.entrySet()) {
-            String fieldName = entry.getKey();
-            Object value = entry.getValue();
-
-            if (value instanceof Integer) {
-                schemaBuilder.field(fieldName, Schema.OPTIONAL_INT32_SCHEMA);
-            } else if (value instanceof Long) {
-                schemaBuilder.field(fieldName, Schema.OPTIONAL_INT64_SCHEMA);
-            } else if (value instanceof Boolean) {
-                schemaBuilder.field(fieldName, Schema.OPTIONAL_BOOLEAN_SCHEMA);
-            } else if (value instanceof Double || value instanceof Float) {
-                schemaBuilder.field(fieldName, Schema.OPTIONAL_FLOAT64_SCHEMA);
-            } else {
-                // default -> string (para nulls, datas, etc)
-                schemaBuilder.field(fieldName, Schema.OPTIONAL_STRING_SCHEMA);
-            }
-        }
-
-        Schema schema = schemaBuilder.build();
-
-
         Map<String, Object> payload = new HashMap<>();
         payload.put("after", data);
         payload.put("before", beforeData);
         payload.put("op", headers.get("operation").toString().substring(0,1).toLowerCase());
 
-        Map<String, Object> newMessage = new HashMap<>();
-        newMessage.put("payload", payload);
+        Schema envelopeSchema = getSchema(data);
+        Struct envelope = new Struct(envelopeSchema);
+        envelope.put("payload", payload);
 
         return record.newRecord(
                 record.topic(),
                 record.kafkaPartition(),
                 record.keySchema(),
                 record.key(),
-                schema,
-                newMessage,
+                envelopeSchema,
+                envelope,
                 record.timestamp()
         );
+    }
+
+    private static Schema getSchema(Map<String, Object> data) {
+        SchemaBuilder schemaFieldsBuilder = SchemaBuilder.struct().name("Fields");
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            String fieldName = entry.getKey();
+            Object value = entry.getValue();
+
+            if (value instanceof Integer) {
+                schemaFieldsBuilder.field(fieldName, Schema.OPTIONAL_INT32_SCHEMA);
+            } else if (value instanceof Long) {
+                schemaFieldsBuilder.field(fieldName, Schema.OPTIONAL_INT64_SCHEMA);
+            } else if (value instanceof Boolean) {
+                schemaFieldsBuilder.field(fieldName, Schema.OPTIONAL_BOOLEAN_SCHEMA);
+            } else if (value instanceof Double || value instanceof Float) {
+                schemaFieldsBuilder.field(fieldName, Schema.OPTIONAL_FLOAT64_SCHEMA);
+            } else {
+                // default -> string (para nulls, datas, etc)
+                schemaFieldsBuilder.field(fieldName, Schema.OPTIONAL_STRING_SCHEMA);
+            }
+        }
+        Schema schemaFields = schemaFieldsBuilder.build();
+
+        SchemaBuilder schemaBuilder = SchemaBuilder.struct().name("Main");
+        schemaBuilder.field("after", schemaFields);
+        schemaBuilder.field("before", schemaFields);
+        schemaBuilder.field("op", Schema.OPTIONAL_STRING_SCHEMA);
+        Schema schema = schemaBuilder.build();
+
+        Schema envelopeSchema = SchemaBuilder.struct()
+                .field("payload", schema) // ou mais elaborado
+                .build();
+
+        return envelopeSchema;
     }
 
     @Override
